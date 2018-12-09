65b81f10e9e93a96394316854097b5539b031514
Antti Partanen <aehparta@iki.fi>
2018-12-09 21:46:20 +0200

    autoload

--- autoload.php
+++ autoload.php
@@ -1,10 +1,8 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    $file = cfg(['paths', 'modules'], null, null, false) . '/' . str_replace('\\', '/', $class) . '.php');
+    $file = cfg(['paths', 'modules'], null, null, false) . '/' . str_replace('\\', '/', $class) . '.php';
     if (file_exists($file)) {
         require_once $file;
     }
-};
-
-echo "moi";
+});
