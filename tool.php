10b084e9c4bd51909595b59da3482a3de5cf6e7d
Antti Partanen <aehparta@iki.fi>
2019-02-14 09:58:37 +0200

    tool_validate to validate

--- tool.php
+++ tool.php
@@ -71,8 +71,8 @@ function tool_call_parse($call, array $args = [], $log = true)
         $args = [];
         foreach ($names as $name) {
             $name = trim($name);
-            if (tool_validate('number', $name)) {
-                tool_validate('int', $name); /* this converts value ($name) to int if it can be done */
+            if (validate('number', $name)) {
+                validate('int', $name); /* this converts value ($name) to int if it can be done */
                 $args[] = $name;
             } else if (!array_key_exists($name, $all_args)) {
                 log_error('Call parsing failed, missing argument "{arg}" for call {call}', ['call' => $call['call'], 'arg' => $name]);
@@ -209,7 +209,7 @@ function tool_call_successful($call, $returned)
         $n     = count($types);
         foreach ($types as $type) {
             if (is_string($type)) {
-                if (!tool_validate($type, $returned, false)) {
+                if (!validate($type, $returned, false)) {
                     $n--;
                 }
             } else if ($type !== $returned) {
@@ -224,7 +224,7 @@ function tool_call_successful($call, $returned)
         $types = is_array($call['fail']) ? $call['fail'] : [$call['fail']];
         foreach ($types as $type) {
             if (is_string($type)) {
-                if (tool_validate($type, $returned, false)) {
+                if (validate($type, $returned, false)) {
                     log_debug('Call failed return value check (matching fail value), call: {call}, returned type: {value}', ['call' => $call['call'], 'value' => gettype($returned)]);
                     return false;
                 }
@@ -267,89 +267,6 @@ function tool_system_find_files(array $filenames, $paths = null, $depth = 2, $fi
     }
 
     return $found;
-}
-
-function tool_validate($type, &$value, $convert = true, $extra = null)
-{
-    if (($type == 'string' || empty($type)) && is_string($value)) {
-        return true;
-    } else if ($type == 'int' && filter_var($value, FILTER_VALIDATE_INT, FILTER_FLAG_ALLOW_OCTAL) !== false) {
-        /* allow "octal" so that string starting with zero is accepted */
-        $value = $convert ? intval($value) : $value;
-        return true;
-    } else if ($type == 'float' && filter_var($value, FILTER_VALIDATE_FLOAT) !== false) {
-        $value = $convert ? floatval($value) : $value;
-        return true;
-    } else if ($type == 'number' && is_numeric($value)) {
-        $value = $convert ? floatval($value) : $value;
-        return true;
-    } else if ($type == 'bool' && is_bool($value)) {
-        return true;
-    } else if ($type == 'null' && is_null($value)) {
-        return true;
-    } else if ($type == 'array' && is_array($value)) {
-        return true;
-    } else if ($type == 'object' && is_object($value)) {
-        return true;
-    } else if ($type == 'email' && filter_var($value, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    } else if ($type == 'ip' && filter_var($value, FILTER_VALIDATE_IP)) {
-        return true;
-    } else if ($type == 'ipv4' && filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-        return true;
-    } else if ($type == 'ipv6' && filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-        return true;
-    } else if ($type == 'url' && filter_var($value, FILTER_VALIDATE_URL)) {
-        return true;
-    } else if ($type == 'datetime') {
-        if (is_a($value, 'DateTime')) {
-            return true;
-        }
-        if (!is_string($value)) {
-            return false;
-        }
-        $t = date_create($value, is_a($extra, 'DateTimeZone') ? $extra : null);
-        if ($t !== false) {
-            $value = $convert ? $t : $value;
-            return true;
-        }
-        return false;
-    } else if ($type == 'timestamp') {
-        /* allow "octal" so that string starting with zero is accepted */
-        $v = filter_var($value, FILTER_VALIDATE_INT, FILTER_FLAG_ALLOW_OCTAL);
-        if ($v === false) {
-            return false;
-        }
-        $v = date_create('@' . $v);
-        if ($v !== false) {
-            $value = $convert ? $v : $value;
-            return true;
-        }
-        return false;
-    } else if ($type == 'fqdn' && tool_validate_fqdn($value) !== false) {
-        return true;
-    } else if ($type == 'fqdn-wildcard' && tool_validate_fqdn($value, true) !== false) {
-        return true;
-    }
-
-    return false;
-}
-
-function tool_validate_fqdn($domain, $allow_wildcard = false)
-{
-    if (!is_string($domain)) {
-        return false;
-    }
-
-    if ($allow_wildcard and substr($domain, 0, 2) == '*.') {
-        $domain = substr($domain, 2);
-    }
-
-    $pattern = '/(?=^.{1,254}$)(^(?:(?!\d|-)[a-zA-Z0-9\-_]{1,63}(?<!-)\.?)+(?:[a-zA-Z]{2,})$)/i';
-    if (!strpbrk($domain, '.')) {
-        return false;
-    }
-    return !empty($domain) && preg_match($pattern, $domain) > 0;
 }
 
 function tool_is_http_request()
