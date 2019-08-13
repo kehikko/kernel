0a600c65f349c5ec4f02c104f12a3d8a5fe5f3d9
Antti Partanen <aehparta@iki.fi>
2019-08-13 15:56:08 +0300

    moved comments around

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -3,11 +3,10 @@ declare (strict_types = 1);
 
 final class CacheTest extends PHPUnit\Framework\TestCase
 {
+    /* do tests using a null cache driver */
     public function testCacheNullDriver(): void
     {
         cfg_init(__DIR__ . '/test-config-cache.yml', null, true);
-
-        /* do tests using a null cache driver */
         $this->assertFalse(cache()->set('key', 'nothing'));
         $this->assertNull(cache()->get('key'));
         $this->assertTrue(cache()->get('key', true));
@@ -18,9 +17,9 @@ final class CacheTest extends PHPUnit\Framework\TestCase
         $this->assertIsIterable(cache()->getMultiple(['1', '2']));
         $this->assertFalse(cache()->has('1'));
         $this->assertTrue(cache()->deleteMultiple(['1', '2']));
-
-        /**
-         * @todo do tests using a "real" driver which runs in memory for the duration of this test
-         **/
     }
+
+    /**
+     * @todo do tests using a "real" driver which runs in memory for the duration of this test
+     */
 }
